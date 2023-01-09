318d8a2b7cbc1f68a2cd960317c55a2a3782362a
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2023-01-09 14:07:36 +0100

    Lägg till alternativen till drycken.

--- Hs/Drink.hs
+++ Hs/Drink.hs
@@ -14,7 +14,7 @@ recipe =
     [ Step "Ta fram en jäshink på 30 liter." []
     , Step "Skär äpplen i bitar och lägg i hinken." [Item "äpplen" 1.5 Kilo]
     , Step
-        "Skiva apelsin med skalet och lägg i hinken."
+        "Skiva apelsin med skalet och lägg i hinken. Apelsinen är till för att ta bort en bismak som kan bli när man fermenterar frukt."
         [Item "apelsin" 1.5 Piece]
     , Step "Häll blåbären i hinken." [Item "blåbär" 0.75 Kilo]
     , Step
@@ -25,23 +25,31 @@ recipe =
         [Item "honung" 375 Gram]
     , Step "Häll i fänkålen i hinken." [Item "fänkålsfrö" 17 Gram]
     , Step
-        "Valfritt. Krydda drycken med tex chili, ingefära, färsk gurkmeja, nejlikor, eller något annat"
-        [Item "hel kryddnejlika" 2 Tablespoon, Item "ingefära" 1 Teaspoon]
-    , Step "Fyll hinken med kallt vatten." []
-    , Step
-        "Sätt på ett tätslutande lock på hinken. Locket kan ha ett vattenlås men behöver det inte."
-        []
-    , Step "Ställ hinken i rumstemperatur." []
-    , Step
-        "Låt jäsa i tre veckor. I början kommer drycken expandera något så var försiktig om lite av drycken skulle rinna ut."
+        "Fyll hinken med kallt vatten så att totalvolymen blir 30 liter. Sätt på ett tätslutande lock på hinken. Locket kan ha ett vattenlås men behöver det inte."
         []
     , Step
-        "Tillsätt honung utblandad med vatten i hinken, och rör om."
+        "Ställ hinken i rumstemperatur och låt jäsa i tre veckor. I början kommer drycken expandera något så var försiktig om lite av drycken skulle rinna ut. Är det kallare än 20 grader så tar jäsningen längre tid, låt då drycken jäsa längre tills den har jäst ut helt."
+        []
+    , Step
+        "Tillsätt honung utblandad med vatten i hinken, och rör om. Honungen är till för att andra fermenteringen i flaskan ska ge viss kolsyra."
         [Item "honung" 150 Gram]
     , Step
         "Låt hinken stå något dygn för att sedimenten ska lägga sig på botten igen."
         []
     , Step "Tappa upp på flaskor. Gärna glasflaskor med patentkork." []
-    , Step "Låt flaskorna stå i rumstemeratur i tre veckor." []
+    , Step
+        "Låt flaskorna stå i rumstemeratur i tre veckor. Ju längre flaskorna står desto mer tydlig blir kolsyran."
+        []
+    ]
+    [ Step "Äpplen går att byta mot tex rabarber som ger en annan smak." []
+    , Step
+        "Olika kryddningsalternativ. Krydda drycken med tex chili, ingefära, färsk gurkmeja, nejlikor, eller något annat gott."
+        []
+    , Step
+        "En Julig variant går att göra genom att krydda med kryddnejlika och ingefära, men den är god även när det inte är Jul."
+        [Item "hel kryddnejlika" 2 Tablespoon, Item "ingefära" 1 Teaspoon]
+    , Step
+        "Under utveckling. En variant med tesmak. Sjud teet i cirka 30 minuter, låt svalna och tillsätt teet med bladen i jäshinken. För att få en fin tefärg på drycken så använd inga bär med starka färger, så som blåbär."
+        [Item "svart te" 0.5 Deciliter]
     ]
     ["Kjell Claeson", "Jonas Claeson"]
