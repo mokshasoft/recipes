b44542d8b10a733f0c8c2e01d09b715c2abcb72e
Jonas Claeson <jonas.cl@protonmail.com>
2024-01-01 10:40:48 +0100

    Uppdatera brödrecept

--- GlutenfrittSurdegsBröd.md
+++ GlutenfrittSurdegsBröd.md
@@ -1,5 +1,5 @@
 # Gluten-fritt surdegsbröd
-Surdegsbröd gjort utan gluten. En limpa.
+Surdegsbröd gjort utan gluten, och utan behov av att hålla en surdegskultur levande. Receptet ger en limpa.
 
 ## Storlek
 1.0 st 
@@ -16,21 +16,21 @@ Surdegsbröd gjort utan gluten. En limpa.
 
 
 ## Steg
-1. Mixa följande i en höghastighetsmixer. Cirka 250 gram totalt.
+1. Mixa följande i en höghastighetsmixer. Det funkar säkert med en vanlig mixer också. Cirka 250 gram totalt.
     - 2.0 dl havre
     - 2.0 dl vit quinoa
     - 0.5 dl psylliumhusk
     - 1.0 tsk salt
     - 1.0 tsk kummin/brödkryddor
-2. Tillsått vatten och probioform till mjölet. Knåda till en deg och lägg i en bunke.
+2. Tillsått vatten och Probioform till mjölet. Knåda till en deg och lägg i en bunke.
     - 3.0 dl vatten
     - 2.0 msk Probioform
 3. Blötlägg bovete.
     - 3.0 dl skalat bovete
-4. Låt stå i rumstemeratur i 24 timmar.
-5. Skölj boveten och knåda in i degen. Lägg tillbaka i bunken så att degen jäser till rätt form.
-6. Låt stå ytterligare 12-24 timmar.
-7. Stjälp upp degen på en plåt och grädda på 220 grader i 30 minuter.
+4. Låt degen och boveten stå i rumstemeratur i 24 timmar.
+5. Skölj boveten noga och knåda in i degen. Lägg tillbaka i bunken så att degen jäser till rätt form.
+6. Låt stå ytterligare 12-24 timmar. Ju mer tid desto mer syrlighet.
+7. Stjälp upp degen på en plåt och grädda på 210 grader i 30 minuter. Ställ en skål med vatten i botten på ugnen.
 8. Pensla brödet med vatten och låt stå i eftervärmen i ugnen.
 
 
--- Hs/GFBread.hs
+++ Hs/GFBread.hs
@@ -9,10 +9,10 @@ recipe =
   Recipe
     "GlutenfrittSurdegsBröd"
     "Gluten-fritt surdegsbröd"
-    "Surdegsbröd gjort utan gluten. En limpa."
+    "Surdegsbröd gjort utan gluten, och utan behov av att hålla en surdegskultur levande. Receptet ger en limpa."
     (Item "" 1 Piece)
     [ Step
-        "Mixa följande i en höghastighetsmixer. Cirka 250 gram totalt."
+        "Mixa följande i en höghastighetsmixer. Det funkar säkert med en vanlig mixer också. Cirka 250 gram totalt."
         [ Item "havre" 2 Deciliter
         , Item "vit quinoa" 2 Deciliter
         , Item "psylliumhusk" 0.5 Deciliter
@@ -20,16 +20,18 @@ recipe =
         , Item "kummin/brödkryddor" 1 Teaspoon
         ]
     , Step
-        "Tillsått vatten och probioform till mjölet. Knåda till en deg och lägg i en bunke."
+        "Tillsått vatten och Probioform till mjölet. Knåda till en deg och lägg i en bunke."
         [Item "vatten" 3 Deciliter, Item "Probioform" 2 Tablespoon]
     , Step "Blötlägg bovete." [Item "skalat bovete" 3 Deciliter]
-    , Step "Låt stå i rumstemeratur i 24 timmar." []
+    , Step "Låt degen och boveten stå i rumstemeratur i 24 timmar." []
     , Step
-        "Skölj boveten och knåda in i degen. Lägg tillbaka i bunken så att degen jäser till rätt form."
+        "Skölj boveten noga och knåda in i degen. Lägg tillbaka i bunken så att degen jäser till rätt form."
         []
-    , Step "Låt stå ytterligare 12-24 timmar." []
     , Step
-        "Stjälp upp degen på en plåt och grädda på 220 grader i 30 minuter."
+        "Låt stå ytterligare 12-24 timmar. Ju mer tid desto mer syrlighet."
+        []
+    , Step
+        "Stjälp upp degen på en plåt och grädda på 210 grader i 30 minuter. Ställ en skål med vatten i botten på ugnen."
         []
     , Step "Pensla brödet med vatten och låt stå i eftervärmen i ugnen." []
     ]
